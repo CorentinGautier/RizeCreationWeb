d35bff69550c15d9fe1fb2aff567e1855b0d124e
CorentinGautier <cortech12@gmail.com>
2019-10-17 21:27:57 +0200

    wtf

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contact.sunrize@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "YVous avez reçu un mail de votre site web, venant de.\n\n"."Here are the details:\n\Nom: $name\n\Email: $email_address\n\Telephone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
